f8cb7390c426c25420554d73fd8e89721f1b7078
KumarAbhinav6660 <kumarabhinav6660@gmail.com>
2022-12-14 17:26:38 +0530

    updated

--- src/routes/route.js
+++ src/routes/route.js
@@ -160,4 +160,42 @@ router.post( "/post-query-2", function (req, res){
 })
 
 
+
+let persons= [
+    {
+    name: "PK",
+    age: 10,
+    votingStatus: false
+ },
+ {
+    name: "SK",
+    age: 20,
+    votingStatus: false
+ },
+ {
+    name: "AA",
+    age: 70,
+    votingStatus: false
+ },
+ {
+    name: "SC",
+    age: 5,
+    votingStatus: false
+ },
+ {
+    name: "HO",
+    age: 40,
+    votingStatus: false
+ }
+ ]
+ 
+
+
+
+
+
+
+
+
+
 module.exports = router;
