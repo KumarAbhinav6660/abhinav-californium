11e18ccba207e0da1a1126c8a26f9d5c6c0e76a3
KumarAbhinav6660 <kumarabhinav6660@gmail.com>
2022-12-27 15:31:30 +0530

    updated

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,6 +1,11 @@
 const authenticate = function(req, req, next) {
     //check the token in request header
     //validate this token
+    let token = req.headers["x-Auth-token"];
+    if (!token) token = req.headers["x-auth-token"];
+  
+    //If no token is present in the request header return error. This means the user is not logged in.
+    if (!token) return res.send({ status: false, msg: "token must be present" });
 
     next()
 }
@@ -10,3 +15,6 @@ const authorise = function(req, res, next) {
     // comapre the logged in user's id and the id in request
     next()
 }
+
+module.exports.authenticate = authenticate
+module.exports.authorise = authorise
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,19 +2,29 @@ const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
 
+const myUserController = require("../controllers/user2Controller")
+const myUserMiddleware = require("../middleware/auth")
+
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
 router.post("/users", userController.createUser)
-
 router.post("/login", userController.loginUser)
-
 //The userId is sent by front end
 router.get("/users/:userId", userController.getUserData)
 router.post("/users/:userId/posts", userController.postMessage)
 
 router.put("/users/:userId", userController.updateUser)
 router.delete('/users/:userId', userController.deleteUser)
+
+
+
+router.post("/myusers", myUserController.createMyUser )
+router.post("/mylogin", myUserController.myloginUser)
+router.get("/myusers/:userId", myUserMiddleware.authenticate, myUserController.getMyUsersData)
+router.put("/myusers/:userId", myUserMiddleware.authenticate, myUserController.updateMyUser)
+router.delete("/myusers/:userId", myUserMiddleware.authenticate, myUserController.deleteMyUser)
+
 
 module.exports = router;
